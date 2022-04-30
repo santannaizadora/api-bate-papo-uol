74c73293a66666f0dc4d26f39bc1636f915c7afa
santannaizadora <santannaizadora@gmail.com>
2022-04-30 18:10:42 -0300

    feat: post in /participants

---
+++ index.js
@@ -0,0 +1,52 @@
+import joi from 'joi';
+import express from 'express';
+import cors from 'cors';
+import { MongoClient, ObjectId } from "mongodb";
+import dayjs from 'dayjs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.listen(5000);
+
+const userSchema = joi.object({
+    name: joi.string().min(1).required()
+});
+
+app.post('/participants', async (req, res) => {
+    const { error } = userSchema.validate(req.body);
+    if (error) {
+        res.status(422).send(error.details.message);
+        return;
+    }
+    const client = new MongoClient(process.env.DB_URL);
+    try {
+        await client.connect();
+        const db = client.db(process.env.DB_NAME);
+        const participantsCollection = db.collection('participants');
+        const messagesCollection = db.collection('messages');
+        const user = await participantsCollection.findOne({ name: req.body.name });
+        if (user) {
+            res.status(409).send('User already exists');
+            return;
+        }
+        const message = {
+            from: req.body.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs().format('YYYY-MM-DD HH:mm:ss:SSS')
+        }
+
+        await participantsCollection.insertOne({ name: req.body.name, lastStatus: Date.now() });
+        await messagesCollection.insertOne(message);
+        res.status(201);
+    } catch (err) {
+        res.status(500).send(err);
+    } finally {
+        await client.close();
+    }
+}
+);
